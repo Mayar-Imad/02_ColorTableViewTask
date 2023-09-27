29b1b35b8609e6371fc4bd4e42919cff48dcf7bf
Mayar-Abdulkareem <mayarimad70@gmail.com>
2023-09-27 12:01:34 +0300

    issues fixed

--- 02_ColorTableViewTask/Controllers/ViewController.swift
+++ 02_ColorTableViewTask/Controllers/ViewController.swift
@@ -23,7 +23,6 @@ class ViewController: UIViewController, NewColorDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //deleteAll()
         fetchColorsArray()
         configureViews()
     }
